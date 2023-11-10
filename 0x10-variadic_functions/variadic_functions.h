6e711e423c25262b04aab7cf9e93e573d6bb61da
pianuel <amanaemmanuel80@gmail.com>
2023-11-10 14:43:14 -0800

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+/* MAIN FUNCTIONS */
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
+#endif 
